c4ae74f41a5ebbbf069d14a9ba8fa8de94026412
Nargiz Mamedova <nargizm7@yandex.ru>
2023-02-20 16:00:40 +0400

    [HW3] registration form added

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { emailValidator } from '../../utils/email-validator.directive';
 
 @Component({
   selector: 'app-registration-form',
@@ -6,4 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration-form.component.scss'],
 })
 export class RegistrationFormComponent {
+  submitted = false;
+  registrationForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['', [Validators.required, emailValidator()]],
+    password: ['', Validators.required]
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  get name() { return this.registrationForm.get('name'); }
+  get email() { return this.registrationForm.get('email'); }
+  get password() { return this.registrationForm.get('password'); }
+
+  onSubmit () {
+    this.submitted = true;
+  }
 }
